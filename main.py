9b745ceca256f82b4a7d6d79131a980dd36f1a70
nkmrohit <neerajmaurya.21mnnit@gmail.com>
2023-08-06 15:32:42 -0400

    Update

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ CLIENT_SECRET = "SRy8Q~kOYAFkLp50PBxLkfe0N0hel_OZUfUSsdvk"
 TENANT_ID = "5eabe8c6-2985-4e34-992a-85fcc1c8b004"
 AUTHORITY = f"https://login.microsoftonline.com/{TENANT_ID}"
 SCOPE = ["User.Read", "openid", "profile"]
-REDIRECT_URI = "http://localhost:5000/callback"
+REDIRECT_URI = "https://mahinm.azurewebsites.net/"
 GROUP_ID = "4c604932-4c47-40d5-81da-420a0e6acaa9"
 
 import webbrowser
@@ -20,7 +20,7 @@ import requests
 
 client_id = CLIENT_ID
 tenant_id = TENANT_ID
-redirect_uri = "http://localhost:8501/"
+redirect_uri = "https://mahinm.azurewebsites.net/"
 scopes = ["https://graph.microsoft.com/.default"]
 authority = f"https://login.microsoftonline.com/{tenant_id}"
 endpoint = "https://graph.microsoft.com/v1.0/me"
@@ -42,7 +42,7 @@ def get_token_from_cache():
 
 def login():
     flow = app.initiate_auth_code_flow(scopes=scopes, state=['somestupidstate'])
-    
+    print(flow)
     if "auth_uri" not in flow:
         return st.write("Failed with token")
     
